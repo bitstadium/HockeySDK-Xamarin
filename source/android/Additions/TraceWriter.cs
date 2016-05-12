2701bdf901eb696e1be400ddfc271581b1ecb5b2
Michael.Dang <michael.dang@xamarin.com>
2016-05-12 13:52:07 -0700

    Delete Unused TraceWriter Source

--- source/android/Additions/TraceWriter.cs
+++ source/android/Additions/TraceWriter.cs
@@ -43,41 +43,16 @@ namespace HockeyApp
             _FilesPath = Constants.FilesPath;
         }
 
-        private static string ClipString(string s)
-        {
-            if ((s != null) && (s.Length > 255))
-                s = s.Substring(0, 255);
-            return s;
-        }
         /// <summary>
         /// Initialize the TraceWriter with a given CrashManagerListener
         /// </summary>
         /// <param name="listener"></param>
         /// <remarks>Thuis is important to use the UserID, Contact and Description from CrashManagerListener.</remarks>
-        public static void Initialize(CrashManagerListener listener)
+        public static void Initialize(CrashManagerListener listener = null)
         {
             _Listener = listener;
             Initialize();
-            if (listener != null)
-            {
-                _IncludeDeviceData = _Listener.IncludeDeviceData();
-                _User = ClipString(listener.UserID) + "";
-                _Contact = ClipString(listener.Contact);
-                if (AllowCachedDescription)
-                    _Description = listener.Description;
-            }
         }
-        public static void WriteTrace(object exception)
-        {
-            WriteTrace(exception, true);
-        }
-        /*
-         from
-           at System.IO.Directory.CreateDirectoriesInternal (System.String path) [0x00000] in <filename unknown>:0 
-         to
-         \tat android.support.v4.app.FragmentManagerImpl.checkStateLoss(FragmentManager.java:1343)
-        */
-        static Regex _StackTraceLine = new Regex(@"\s*at\s*(\S+)\s*\(([^\)]*)\)");
 
         /// <summary>
         /// Writes the given object (usually an exception) to disc so that it can be picked up by the CrashManager and send to Hockeyapp.
@@ -87,7 +62,7 @@ namespace HockeyApp
         /// <remarks>This method controls exactly what is written to disc.  Its really a translation of the ExceptionHandler.saveException
         /// This method doesn't call any java if the exception object isn't a java class so it is safe to call when the mono runtime is no 
         /// longer able to invoke JNI methods, for example in the case of a crash in the pure c# managed space.</remarks>
-        public static void WriteTrace(object exception, bool terminate)
+        public static void WriteTrace(object exception, bool terminate = true)
         {
             if (exception is Java.Lang.Exception)
                 ExceptionHandler.SaveNativeException(exception as Java.Lang.Exception, exception.ToString(), Java.Lang.Thread.CurrentThread(), _Listener);
@@ -99,44 +74,6 @@ namespace HockeyApp
                 Process.GetCurrentProcess ().Kill ();
                 Environment.Exit (10);
             }
-        }
-
-        /// <summary>
-        /// Writes the given exception to disc using the standard Exceptionhandler.  Does not terminate the process, so it could be suitable
-        /// for reporting non-fatal logs that upload on the next CrashManager execution
-        /// </summary>
-        /// <param name="exception">The exception to write.</param>
-        /// <remarks>All features from CrashManagerListener are used (including informations like UserId, Contact and Description).</remarks>
-        public static void WriteTraceAndInfo(Exception exception)
-        {
-            WriteTrace(exception, false);
-        }
-
-        public static string Logcat(string appTag)
-        {
-            string description = "";
-
-            try
-            {
-                var p = new Process();
-                // Redirect the output stream of the child process.
-                p.StartInfo.UseShellExecute = false;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.FileName = "logcat";
-                if (_Version >= 16)
-                    p.StartInfo.Arguments = "-d";
-                else
-                    p.StartInfo.Arguments = "-d ActivityManager:I " + appTag + ":D *:S";
-                p.Start();
-                description = p.StandardOutput.ReadToEnd();
-                p.WaitForExit();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Couldnt get logcat: {0}", e);
-            }
-
-            return description;
         }
 
     }
