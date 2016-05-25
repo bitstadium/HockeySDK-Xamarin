19b766a762350fe8a21869772638632c2eb425a2
Michael.Dang <michael.dang@xamarin.com>
2016-05-25 14:40:24 -0700

    Consolidate documentation to exposed static library

--- source/HockeySDK.iOS/PlatformMetricsManager.cs
+++ source/HockeySDK.iOS/PlatformMetricsManager.cs
@@ -3,24 +3,10 @@ using HockeyApp.iOS;
 
 namespace HockeyApp
 {
-	/// <summary>
-	/// This is the HockeySDK module that handles users, sessions and events tracking.
-	/// </summary>
-	/// <description>
-	/// Unless disabled, this module automatically tracks users and session of your app to give you better insights about how your app is being used. Users are tracked in a completely anonymous way without collecting any personally identifiable information.
-	/// 
-	/// Before starting to track events, ask yourself the questions that you want to get answers to. For instance, you might be interested in business, performance/quality or user experience aspects. Name your events in a meaningful way and keep in mind that you will use these names when searching for events in the HockeyApp web portal.
-	/// 
-	/// It is your reponsibility to not collect personal information as part of the events tracking or get prior consent from your users as necessary.
-	/// </description>
 	internal class PlatformMetricsManager : IPlatformMetricsManager
 	{
 		public PlatformMetricsManager() {}
 
-		/// <summary>
-		/// This method allows to track an event that happened in your app. Remember to choose meaningful event names to have the best experience when diagnosing your app in the HockeyApp web portal.
-		/// </summary>
-		/// <param name="eventName">The event’s name as a string.</param>
 		public void TrackEvent(string eventName)
 		{
 			BITHockeyManager.SharedHockeyManager.MetricsManager.TrackEvent(eventName);
